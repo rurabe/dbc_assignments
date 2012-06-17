6a74a5af07b1d1cc8a6e0c51f0ebbe23ed6fcfc3
Ryan Urabe <rurabe@gmail.com>
2012-06-17 16:41:31 -0700

    pokerbot updated

--- poker/pokerbot.rb
+++ poker/pokerbot.rb
@@ -4,19 +4,20 @@
 
 #write a function that takes two arrays-- one that describes your two "hole" cards, and another that describes the five community cards-- and determine your odds of winning the hand against another single player (ie. another player with two of the remaining 45 cards selected at random).
 
-@hand =  [{"name"=>5, "suit"=>"Club", "value"=>6, "string"=>"5 of Clubs"},
-         {"name"=>"King", "suit"=>"Heart", "value"=>12, "string"=>"King of Hearts"}]
+@hand =  [{"name"=>7, "suit"=>"Spade", "value"=>7, "string"=>"5 of Clubs"},
+         {"name"=>"King", "suit"=>"Spade", "value"=>13, "string"=>"King of Hearts"}]
 		
-@board = [{"name"=>5, "suit"=>"Spade", "value"=>5, "string"=>"5 of Spades"},
+@board = [{"name"=>6, "suit"=>"Spade", "value"=>6, "string"=>"5 of Spades"},
 		     {"name"=>"King", "suit"=>"Spade", "value"=>13, "string"=>"King of Spades"},
-		     {"name"=>"Jack", "suit"=>"Diamond", "value"=>11, "string"=>"Jack of Diamonds"},
-	       {"name"=>5, "suit"=>"Heart", "value"=>5, "string"=>"5 of Hearts"},
-		     {"name"=>"King", "suit"=>"Diamond", "value"=>13, "string"=>"King of Diamonds"}]
+		     {"name"=>"Jack", "suit"=>"Spade", "value"=>11, "string"=>"Jack of Diamonds"},
+	       {"name"=>5, "suit"=>"Spade", "value"=>5, "string"=>"5 of Hearts"},
+		     {"name"=>"King", "suit"=>"Heart", "value"=>13, "string"=>"King of Diamonds"}]
 		     
 def best_hand(hand,board)
-  @hand = hand
-  @board = board
   @all_cards = hand + board
+  
+  counts_of_values(hand,board)
+  counts_of_suits(hand,board)
   
   #four of a kind -- WORKING
   if @counts_of_values.values.include?(4)
@@ -25,24 +26,34 @@ def best_hand(hand,board)
   
   #best trips -- WORKING
   if @counts_of_values.values.include?(3)
-    @trips = @counts_of_values.keep_if {|value,count| count == 3 }.keys.to_a.max
+    @trips = []
+    counts_temp = @counts_of_values
+    counts_keys = counts_temp.keep_if {|value,count| count == 3 }.keys.sort
+    counts_keys.size.times do
+      @trips << counts_keys.pop
+    end
   end
   
-  #pairs -- NEEDS WORK
+  #pairs -- WORKING
   if @counts_of_values.values.include?(2)
-    counts_temp = @counts_of_values
     @pairs = []
-    while counts_temp.values.include?(2) == true
-      @pairs << counts_temp.keep_if {|value,count| count == 2 }.keys.sort.pop!
-      print counts_temp
-      print @pairs
+    counts_temp = @counts_of_values
+    counts_keys = counts_temp.keep_if {|value,count| count == 2 }.keys.sort
+    counts_keys.size.times do
+      @pairs << counts_keys.pop
     end
   end
   
   #flush -- NEEDS WORK
-  if @counts_of_suits.values.include?(5)
-    suits_temp = @counts_of_suits
-    @flush_suit = suits_temp.key(5)
+  if @counts_of_suits.values.sort[-1] > 4
+    @flush_suit = @counts_of_suits.values.sort[-1]
+    @flush_values = []
+    @all_cards.each do |h|
+      if h["suit"] = @flush_suit
+        @flush_values << h["value"]
+        print @flush_values
+      end
+    end
     @flush_values
   end
   
