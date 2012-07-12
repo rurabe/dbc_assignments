c99ef96776e2e6c8c636d5becdd1aa5754b00be0
rurabe <rurabe@gmail.com>
2012-07-11 23:22:23 -0700

    still banging my head on jasmine

--- week4/javascript/stack_jasmine/stack.js
+++ week4/javascript/stack_jasmine/stack.js
@@ -1,5 +1,4 @@
-var stack = function() {
-	var stack = {
+var stack = {
 		data: [],
 		size: function(){
 			return this.data.length;
@@ -7,9 +6,13 @@ var stack = function() {
 	
 		add: function(arg){
 			this.data.push(arg)
+		},
+		
+		remove: function(){
+			if (this.size() === 0){
+				throw new Error("Stack is already empty!")
+			}else{
+				return this.data.pop();				
+			}
 		}
-	};
-
-	return stack;
-	
 };
--- week4/javascript/stack_jasmine/stack_spec.js
+++ week4/javascript/stack_jasmine/stack_spec.js
@@ -1,4 +1,8 @@
 describe("The stack", function() {
+	
+	beforeEach(function() {
+	    stack.data = []
+	  });
 
   it("should be an object", function() {
     expect(stack).toEqual(jasmine.any(Object));
@@ -6,11 +10,40 @@ describe("The stack", function() {
 
 
 	it("should have a size", function(){
-		expect(stack.size).toEqual(0)
-	})
+		expect(stack.size()).toEqual(0);
+	});
 	
 	it("should have an add method", function(){
-		stack.add('item')
-		expect(stack.size).toEqual(1)
-	})
+		stack.add('item');
+		expect(stack.size()).toEqual(1);
+	});
+	
+	it("should increment by 1 with an existing stack", function(){
+		expect(stack.size()).toEqual(0);
+		stack.add('item1');
+		stack.add('item2');
+		stack.add('item3');
+		stack.add('item4');
+		expect(stack.size()).toEqual(4);
+		stack.add('item5');
+		expect(stack.size()).toEqual(5);
+	});
+	
+	it("should have a remove method", function(){
+		stack.add('item');
+		expect(stack.size()).toEqual(1);
+		stack.remove();
+		expect(stack.size()).toEqual(0);
+	});
+	
+	it("should return the most recently added item from the remove function", function(){
+		stack.add('item');
+		expect(stack.size()).toEqual(1);
+		expect(stack.remove()).toEqual('item');
+	});
+	
+	it("should raise an error if you try to remove from an empty stack", function(){
+		expect(stack.remove()).toThrow(new Error("Stack is already empty!"));
+	});
+	
 });
