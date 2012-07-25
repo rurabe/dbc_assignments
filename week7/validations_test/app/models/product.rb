591f830c08d81076c0f9d0224f36c64b65f88b8c
rurabe <rurabe@gmail.com>
2012-07-25 12:26:59 -0700

    finished validations testing

--- week7/validations_test/app/models/product.rb
+++ week7/validations_test/app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :imageurl, :price, :size, :title
   
-  validates_presence_of :title, :price, :description, :imageurl, :size 
+  validates_presence_of :title, :price, :description, :imageurl, :size
+  validates :title, :length => { :maximum => 50 }, :uniqueness => { :case_sensitive => false }
+  validates :description, :length => { :maximum => 250 }
+  validates :price, :format => { :with => /[\d\,]*\.\d{2}$/ }
+  validates :imageurl, :format => { :with => /#{URI::regexp}\.(jpg|png|gif)$/i }
+  validates :size, :inclusion => { :in => %w(small medium large) }
 end
