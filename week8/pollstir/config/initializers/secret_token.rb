75284585c6f0b3cb36a32a0b31411766b89f8189
rurabe <rurabe@gmail.com>
2012-07-30 10:52:42 -0700

    new rails app

---
+++ week8/pollstir/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollstir::Application.config.secret_token = '4ebaae52a300fa2173cca31fc05d839a5ff84a996d37637374027a0c8d6590a1b66100c838cef142406b20aa76dfd6f8425256e03d3504f451163768f648d974'
